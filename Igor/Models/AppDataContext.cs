ac668b9e11f3e61b7b6568fba035d6badd478646
apartigor <contact.jojigor@gmail.com>
2024-10-16 10:08:49 -0300

    UPDATE

---
+++ Igor/Models/AppDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Igor.Models;
+
+public class AppDataContext : DbContext
+{
+    public DbSet<Funcionario> Funcionarios { get; set; }
+    public DbSet<Folha> Folhas { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=Nicolas_Igor.db");
+    }
+}
---
+++ Igor/Models/Funcionario.cs
@@ -0,0 +1,8 @@
+﻿namespace Igor.Models;
+
+public class Funcionario
+{
+    public int funcionarioId { get; set; }
+    public string? nome { get; set; }
+    public string? cpf { get; set; }
+}
